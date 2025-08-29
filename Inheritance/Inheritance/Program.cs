e73bc29b9b5515fd0e69328d941e3b9ed893e502
ACT-STUDENT <ACT-STUDENT@LAB3-PC39>
2025-08-29 10:50:53 +0800

    CODE

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Input Car Brand: ");
+            string brand = Console.ReadLine();
+
+            Car car1 = new Car { Brand = brand };
+            car1.Car1();
+
+
+            Console.Write("Input Bicycle Brand: ");
+            string brand1 = Console.ReadLine();
+
+            Bicycle bicyle1 = new Bicycle { Brand1 = brand1 };
+            bicyle1.Bicycle1();
+
+
+            Console.Write("Input Boat Brand: ");
+            string brand2 = Console.ReadLine();
+
+            Boat boat1 = new Boat { Brand2 = brand2 };
+            boat1.Boat1();
+
+            Console.WriteLine("YOUR CHOSEN BRAND ARE: " + brand + " " + brand1 + " " + brand2);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Vehicle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Vehicle
+    {
+        public string Brand ;
+
+    }
+    class Car : Vehicle
+    {
+        public string Brand {  get; set; }
+        
+        public void Car1()
+        {
+            Console.WriteLine("Brand: " + Brand);
+        }
+    }
+    class Bicycle : Vehicle
+    {
+        public string Brand1 { get; set; }
+
+        public void Bicycle1()
+        {
+            Console.WriteLine("Brand: " + Brand1);
+        }
+    }
+    class Boat : Vehicle
+    {
+        public string Brand2 { get; set; }
+
+        public void Boat1()
+        {
+            Console.WriteLine("Brand: " + Brand2);
+        }
+    }
+}
