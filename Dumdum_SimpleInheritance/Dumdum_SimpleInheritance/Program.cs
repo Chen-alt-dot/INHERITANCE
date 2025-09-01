75f3a9ba6179069cebf4b6de4d7e3791cfc1f45c
ACT-STUDENT <ACT-STUDENT@LAB3-PC39>
2025-09-01 10:52:53 +0800

    Code

---
+++ Dumdum_SimpleInheritance/Dumdum_SimpleInheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dumdum_SimpleInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of Basic Info and Regular Student
+            Student student1 = new Student();
+            student1.Name = "Christian Benedict P. Dumdum";
+            student1.Program = "BSIT";
+            RegularStudent regularStudent1 = new RegularStudent();
+            regularStudent1.Section = "IT301";
+            student1.BasicInfo(); // Inherited Method
+            regularStudent1.SectionEnrolled(); // Regular Student Specific Method
+
+            Console.WriteLine("");
+
+            //Create instances of Basic Info and Irregular Student
+            IrregularStudent irregularstudent1 = new IrregularStudent();
+            irregularstudent1.Name = "Gian Marty Pepito";
+            irregularstudent1.Program = "BSIT";
+            irregularstudent1.UnitsEnrolled = 18;
+            irregularstudent1.BasicInfo(); // Inherited Method
+            irregularstudent1.EnrolledSemUnits(); // Irregular Student Specific Method
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dumdum_SimpleInheritance/Dumdum_SimpleInheritance/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dumdum_SimpleInheritance
+{
+    //Name: Christian Benedict P. Dumdum
+    //Section: IT301
+    //EXER_06: Simple Inheritance
+
+
+    class Student  // Base Class
+    {
+
+        // Base Class Properties
+       public string Name { get; set; }
+       
+        public string Program { get; set; }
+
+        // Base Class Method
+        public void BasicInfo()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Program: " + Program);
+        }
+    }
+    class RegularStudent : Student  // Derived Class
+    {
+        // Property Specificically for RegularStudent
+        public string Section { get; set; }
+
+        // Method Specifically for Regular Student
+        public void SectionEnrolled()
+        {
+            Console.WriteLine("Section Enrolled: " + Section);
+        }
+    }
+    class IrregularStudent : Student  // Derived Class
+    {
+        // Property Specifically for Irregular Student
+        public int UnitsEnrolled { get; set; }
+
+        // Method Specifically for Irregular Student
+        public void EnrolledSemUnits()
+        {
+            Console.WriteLine("Enrolled Sem Units: " + UnitsEnrolled);
+        }
+    }
+}
