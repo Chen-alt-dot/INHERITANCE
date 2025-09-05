d437ce8d36af2a036dcb9e93d50f452b900650ef
ACT-STUDENT <ACT-STUDENT@LAB3-PC39>
2025-09-05 10:56:24 +0800

    Code

---
+++ Dumdum_InheritanceWithConstructor/Dumdum_InheritanceWithConstructor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dumdum_InheritanceWithConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Regular Student Sign
+            Console.WriteLine("-----Regular Student-----");
+            //Instantiate an Object for Regular Student Derived Class
+            RegularStudent student1 = new RegularStudent("IT301" , "Christian Benedict P. Dumdum", "BSIT");
+
+            //Methods for Regular Student Derived Class
+            student1.BasicInfo();
+            student1.SectionEnrolled();
+
+            Console.WriteLine(" ");
+
+            //Irregular Student Derived Class
+            Console.WriteLine("-----Regular Student-----");
+
+            //Instantiate and object for Irregular Student Derived Class
+            IrregularStudent student2 = new IrregularStudent("18", "Gian Marty Pepito", "BSIT");
+
+            //Methods for Irregular Student Derived Class
+            student2.BasicInfo();
+            student2.EnrolledSemUnits();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dumdum_InheritanceWithConstructor/Dumdum_InheritanceWithConstructor/Student.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dumdum_InheritanceWithConstructor
+{
+    // Name: Christian Benedict P. Dumdum
+    // Section: IT301
+    // EXER_07: Simple Inheritance with Constructor
+    class Student  // Base Class
+    {
+        //Fields of the Base Class
+        public string name;
+        public string program;
+
+        //Constructor of the Base Class
+        public Student(string aName, string aProgram)
+        {
+            name = aName;
+            program = aProgram;
+        }
+        //Method of the Base Class
+        public void BasicInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Program: " + program);
+        }
+    }
+
+    class RegularStudent : Student // Derived Class
+    {
+        public string section; //Field of the Regular Student Derived Class
+
+        // Constructor of the Regular Student Derived Class
+        public RegularStudent(string aSection , string aName, string aProgram ) : base( aName, aProgram )
+        {
+            section = aSection;
+        }
+        
+        // Method of the Regular Student Derived Class
+        public void SectionEnrolled()
+        {
+            Console.WriteLine("Section: " + section);
+        }
+    }
+    class IrregularStudent : Student // Derived Class
+    {
+        // Field of the Irregular Student Derived Class
+        public string unitsenrolled;
+
+        // Constructor of the Irregular Student Derived Class
+        public IrregularStudent(string aUnitsEnrolled, string aName, string aProgram) : base( aName, aProgram)
+        {
+            unitsenrolled = aUnitsEnrolled;
+        }
+        //Method of the Irregular Student Derived Class
+        public void EnrolledSemUnits()
+        {
+            Console.WriteLine("UnitsEnrolled: " + unitsenrolled);
+        }
+    }
+}
