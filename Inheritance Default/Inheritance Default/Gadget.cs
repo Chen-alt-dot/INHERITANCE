82cc6cef52e548eff8164ff9c27c560799e8d939
ACT-STUDENT <ACT-STUDENT@LAB3-PC39>
2025-09-08 10:22:52 +0800

    Code

---
+++ Inheritance Default/Inheritance Default/Gadget.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Default
+{
+    class Gadget
+    {
+        public string size;
+
+        public Gadget()
+        {
+            size = "Big";
+        }
+        public void DisplaySpeaker()
+        {
+            Console.WriteLine("Size: " + size);
+        }
+
+    }
+    class BluetoothSpeaker : Gadget
+    {
+        public string brand;
+
+        public BluetoothSpeaker()
+        {
+            brand = "Panasonic";
+        }
+        public void DisplayBluetooth()
+        {
+            Console.WriteLine("Brand: " + brand);
+        }
+    }
+}
+
---
+++ Inheritance Default/Inheritance Default/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Default
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----Bluestooth Speaker----");
+            Gadget gadget1 = new Gadget();
+            gadget1.DisplaySpeaker();
+            BluetoothSpeaker blue1 = new BluetoothSpeaker();
+            blue1.DisplayBluetooth();
+
+            Console.ReadKey();
+        }
+    }
+}
